f62637d6b4e97757764281f5f6e53c6a1341fbc3
Tammy <tammycwong@gmail.com>
2021-04-08 19:23:52 -0400

    codealong

--- src/Parent.js
+++ src/Parent.js
@@ -5,13 +5,21 @@ import Child from "./Child";
 function Parent() {
   const randomColor = getRandomColor();
   const [color, setColor] = useState(randomColor);
+  
+  function handleChangeColor(){
+    const newRandomColor = getRandomColor();
+    setColor(newRandomColor); 
+  }
 
   return (
     <div className="parent" style={{ backgroundColor: color }}>
-      <Child />
-      <Child />
+    
+      <Child onChangeColor={handleChangeColor}/>
+      <Child onChangeColor={handleChangeColor}/>
     </div>
   );
 }
-
+// Deliverable 1: when either child is clicked, parent changes color
+// pass handleChangeColor as a PROP to both child components
+// child's onChangeColor's function is SAME function obj as parent's handleChangeColor
 export default Parent;
